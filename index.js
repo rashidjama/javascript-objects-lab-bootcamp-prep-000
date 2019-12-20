51d68e5f794448837d47c069ecfb41b8ae2d3027
Rashid Jama <rashidjama5@gmail.com>
2019-12-20 04:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key];
   return newObj;
 }
+function  destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
