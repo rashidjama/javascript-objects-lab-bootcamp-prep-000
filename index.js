66c8e238d3f6ae4a2c10c336f64446728e25b9a1
Rashid Jama <rashidjama5@gmail.com>
2019-12-20 03:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, key, value)
+  return Object.assign({},{[key]: value})
 }
